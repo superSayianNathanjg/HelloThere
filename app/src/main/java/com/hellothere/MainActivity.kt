2bfbe129b4b298d14d0ffd0ff0fc4b2facde4836
superSayianNathanJG <L_eonarder123___>
2021-02-07 23:17:14 +1100

    Finally got app bar working...

--- app/src/main/java/com/hellothere/MainActivity.kt
+++ app/src/main/java/com/hellothere/MainActivity.kt
@@ -2,59 +2,42 @@ package com.hellothere
 
 import android.media.MediaPlayer
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
+import android.view.*
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.navigation.NavigationView
-import kotlinx.coroutines.awaitAll
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val firstFrag = ObiWanFrag()
         val secondFrag = GeneralGerviousFrag()
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
 
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.frame_layout_fragment, firstFrag)
             commit()
         }
 
-        findViewById<Button>(R.id.obi_wan_button).setOnClickListener {
 
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.frame_layout_fragment, firstFrag)
-                addToBackStack(null)
-                commit()
-            }
-        }
-
-        findViewById<Button>(R.id.general_grevious_button).setOnClickListener {
-
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.frame_layout_fragment, secondFrag)
-                addToBackStack(null)
-                commit()
-
-            }
-
-        }
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
-
+        // When top bar is clicked, open draw.
         findViewById<MaterialToolbar>(R.id.topAppBar).setNavigationOnClickListener {
             drawerLayout.open()
         }
 
 
+        // When top bar item is clicked. Close draw.
         findViewById<NavigationView>(R.id.navigationView).setNavigationItemSelectedListener { menuItem ->
             menuItem.isChecked = true
             drawerLayout.close()
             true
         }
     }
+
+
 
     fun soundHelloThere(view: View) {
         val mediaPlayer: MediaPlayer = MediaPlayer.create(this, R.raw.hello_there_sound)
@@ -73,16 +56,27 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawerLayout)
+        when (item.itemId) {
+            R.id.menu1 -> {
+                val fragOne: ObiWanFrag = ObiWanFrag()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.frame_layout_fragment, fragOne)
+                    .commit()
+                Toast.makeText(this, "FUCK", Toast.LENGTH_LONG).show()
+            }
+            R.id.menu2 -> {
+                val secondFrag: GeneralGerviousFrag = GeneralGerviousFrag()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.frame_layout_fragment, secondFrag)
+                    .commit()
+
+                Toast.makeText(this, "FUCK This", Toast.LENGTH_LONG).show()
+            }
+        };   drawerLayout.close()
+        return true
+    }
+
 }
-
-
-//        findViewById<Button>(R.id.hello_there).setOnClickListener(View.OnClickListener {
-////            val intent = Intent(this, SecondActivity::class.java)
-////            startActivity(intent)
-
-//        })
-
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        menuInflater.inflate(R.menu.menu_toolbar, menu)
-//        return true
-//    }
--- app/src/main/java/com/hellothere/ObiWanFrag.kt
+++ app/src/main/java/com/hellothere/ObiWanFrag.kt
@@ -1,5 +1,9 @@
 package com.hellothere
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class ObiWanFrag : Fragment(R.layout.fragment_obi_wan)
