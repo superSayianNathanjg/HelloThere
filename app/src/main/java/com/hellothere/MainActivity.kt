922c787582b80841729948d5ea2636b395f13c9f
superSayianNathanJG <L_eonarder123___>
2021-02-14 18:39:48 +1100

    Wasted a day, but changed to SoundPool from MediaPlayer

--- app/src/main/java/com/hellothere/MainActivity.kt
+++ app/src/main/java/com/hellothere/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.hellothere
 
-import android.media.MediaPlayer
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.SoundPool
+import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
@@ -10,35 +13,102 @@ import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var mediaPlayer: MediaPlayer // Plays the audio
     private lateinit var drawerLayout: DrawerLayout // Drawer menu
+    private lateinit var soundPool: SoundPool
 
     /* Fragments */
-
     private val fragOne: ObiWanFrag = ObiWanFrag()
     private val fragTwo: GeneralGreviousFrag = GeneralGreviousFrag()
     private val fragThree: PapaPalpsFrag = PapaPalpsFrag()
     private val fragFour: DarthVaderFrag = DarthVaderFrag()
     private val fragAbout: AboutFragment = AboutFragment()
 
+    /* Buttons */
+    // Obi Wan
+    private var helloThere: Int = 0
+    private var highGround: Int = 0
+    private var goodJob: Int = 0
+    private var goodBye: Int = 0
+    private var halfShip: Int = 0
+    private var oldKenobi: Int = 0
+
+    // Grevious
+    private var generalKenobi: Int = 0
+    private var situation: Int = 0
+    private var coughing: Int = 0
+
+    // Palpatine
+    private var doIt: Int = 0
+    private var order66: Int = 0
+    private var senate: Int = 0
+    private var goodPalpatine: Int = 0
+    private var weakPalpatine: Int = 0
+
+    // Darth Vader
+    private var iAmYourFather: Int = 0
+    private var apologyAccepted: Int = 0
+    private var lackOfFaith: Int = 0
+    private var yesVader: Int = 0
+    private var dontFailMe: Int = 0
+    private var noooVader: Int = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         drawerLayout = findViewById(R.id.drawerLayout)
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.hello_there_sound)
-        mediaPlayer.start()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val audioAttributes: AudioAttributes? = AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build()
+            soundPool = SoundPool.Builder()
+                    .setMaxStreams(5)
+                    .setAudioAttributes(audioAttributes)
+                    .build()
+        } else {
+            soundPool = SoundPool(5, AudioManager.STREAM_MUSIC, 0)
+        }
 
+        // Loading SoundPool
+        // Obi Wan
+        helloThere = soundPool.load(this, R.raw.hello_there_sound, 1)
+        highGround = soundPool.load(this, R.raw.high_ground_kenobi, 1)
+        goodJob = soundPool.load(this, R.raw.good_job_kenobi, 1)
+        goodBye = soundPool.load(this, R.raw.goodbye_kenobi, 1)
+        halfShip = soundPool.load(this, R.raw.half_ship_kenobi, 1)
+        oldKenobi = soundPool.load(this, R.raw.kenobi_old, 1)
+
+        // Grevious
+        generalKenobi = soundPool.load(this, R.raw.general_kenobi_sound,1)
+        situation = soundPool.load(this, R.raw.general_grevious_situation,1)
+        coughing = soundPool.load(this, R.raw.grevious_coughing ,1)
+
+        // Palpatine
+        doIt = soundPool.load(this, R.raw.do_it_trimmed , 1)
+        order66 = soundPool.load(this,R.raw.order66_palpatine , 1)
+        senate = soundPool.load(this,R.raw.senate_palpatine , 1)
+        goodPalpatine = soundPool.load(this,R.raw.good_palpatine , 1)
+        weakPalpatine = soundPool.load(this, R.raw.too_weak_palpatine , 1)
+
+        // Darth Vader
+       iAmYourFather = soundPool.load(this,R.raw.i_am_your_sound , 1)
+       apologyAccepted = soundPool.load(this, R.raw.apology_vader , 1)
+       lackOfFaith = soundPool.load(this,R.raw.faith_vader , 1)
+       yesVader = soundPool.load(this, R.raw.yes_vader, 1)
+       dontFailMe = soundPool.load(this, R.raw.fail_me_vader, 1)
+       noooVader = soundPool.load(this,R.raw.nooo , 1)
+
+        /* Navigation */
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.frame_layout_fragment, fragOne)
             commit()
         }
-
         // When top bar is clicked, open draw.
         findViewById<MaterialToolbar>(R.id.topAppBar).setNavigationOnClickListener {
             drawerLayout.open()
         }
-
         // When top bar item is clicked. Close draw.
         findViewById<NavigationView>(R.id.navigationView).setNavigationItemSelectedListener { menuItem ->
             menuItem.isChecked = true
@@ -49,76 +119,139 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(view: View) {
-        mediaPlayer = MediaPlayer.create(this, R.raw.hello_there_sound)
-        if (view.id == R.id.hello_there || view.id == R.id.gifImageViewObiWan) {
-            playSound(R.raw.hello_there_sound)
-        }
-        else if (view.id == R.id.older_kenobi) {
-            playSound(R.raw.kenobi_old)
+        when (view.id) {
+            // Obi Wan
+            R.id.hello_there -> {
+                soundPool.play(helloThere, 1F, 1F, 0, 0, 1F)
+            }
+            R.id.high_ground_obi -> {
+                soundPool.play(highGround, 1F, 1F, 0, 0, 1F)
+            }
+            R.id.good_job_obi -> {
+                soundPool.play(goodJob, 1F, 1F, 0, 0, 1F)
+            }
+            R.id.goodbye_obi -> {
+                soundPool.play(goodBye, 1F, 1F, 0, 0, 1F)
+            }
+            R.id.half_a_ship_obi -> {
+                soundPool.play(halfShip, 1F, 1F, 0, 0, 1F)
+            }
+            R.id.older_kenobi -> {
+                soundPool.play(oldKenobi, 1F, 1F, 0, 0, 1F)
+            }
+            // Grevious
+            R.id.general_kenobi -> {
+                soundPool.play(generalKenobi, 1F, 1F, 0, 0, 1F)
+            }
+            R.id.grevious_situation -> {
+            soundPool.play(situation, 1F, 1F, 0, 0, 1F)
+            }
+            R.id.grevious_coughing -> {
+            soundPool.play(coughing, 1F, 1F, 0, 0, 1F)
+            }
+            // Palpatine
+            R.id.do_it -> {
+                soundPool.play(doIt, 1F, 1F, 0, 0, 1F)
+            }
+            R.id.order_66_palpatine -> {
+                soundPool.play(order66, 1F, 1F, 0, 0, 1F)
+            }
+            R.id.senate -> {
+                soundPool.play(senate, 1F, 1F, 0, 0, 1F)
+            }
+            R.id.good_palpatine -> {
+                soundPool.play(goodPalpatine, 1F, 1F, 0, 0, 1F)
+            }
+            R.id.weak_palpatine -> {
+                soundPool.play(weakPalpatine, 1F, 1F, 0, 0, 1F)
+            }
         }
 
-        // Grevious
-        else if (view.id == R.id.general_kenobi || view.id == R.id.gifImageViewGrevious) {
-            playSound(R.raw.general_kenobi_sound)
-        }else if (view.id == R.id.grevious_coughing) {
-            playSound(R.raw.grevious_coughing)
-        }
-        else if (view.id == R.id.grevious_situation) {
-            playSound(R.raw.general_grevious_situation)
-        }
 
-
-        // Palpatine
-        else if (view.id == R.id.do_it || view.id == R.id.palpatineGif) {
-            playSound(R.raw.do_it_trimmed)
-        }
-        else if (view.id == R.id.order_66_palpatine) {
-            playSound(R.raw.order66_palpatine)
-        }
-        else if (view.id == R.id.senate) {
-            playSound(R.raw.senate_palpatine)
-        }
-
-        // Vader
-        else if (view.id == R.id.i_am_your_button || view.id == R.id.i_am_your_father_gif) {
-            playSound(R.raw.i_am_your_sound)
-        }
-        else if (view.id == R.id.lack_of_faith_vader) {
-            playSound(R.raw.faith_vader)
-        }
-        else if (view.id == R.id.yes_vader) {
-            playSound(R.raw.yes_vader)
-        }
-        else if (view.id == R.id.fail_me_vader) {
-            playSound(R.raw.fail_me_vader)
-        }
-        else if (view.id == R.id.apology_vader) {
-            playSound(R.raw.apology_vader)
-        }
-        else if (view.id == R.id.noooo) {
-            playSound(R.raw.nooo)
-        }
+//        if (view.id == R.id.hello_there || view.id == R.id.gifImageViewObiWan) {
+//            playSound(R.raw.hello_there_sound)
+//
+//        }
+//        else if (view.id == R.id.high_ground_obi) {
+//            playSound(R.raw.high_ground_kenobi)
+//        }
+//        else if (view.id == R.id.good_job_obi) {
+//            playSound(R.raw.good_job_kenobi)
+//        }
+//        else if (view.id == R.id.goodbye_obi) {
+//            playSound(R.raw.goodbye_kenobi)
+//        }
+//        else if (view.id == R.id.half_a_ship_obi) {
+//            playSound(R.raw.half_ship_kenobi)
+//        }
+//        else if (view.id == R.id.older_kenobi) {
+//            playSound(R.raw.kenobi_old)
+//        }
+//
+//        // Grevious
+//        else if (view.id == R.id.general_kenobi || view.id == R.id.gifImageViewGrevious) {
+//            playSound(R.raw.general_kenobi_sound)
+//        }else if (view.id == R.id.grevious_coughing) {
+//            playSound(R.raw.grevious_coughing)
+//        }
+//        else if (view.id == R.id.grevious_situation) {
+//            playSound(R.raw.general_grevious_situation)
+//        }
+//
+//
+//        // Palpatine
+//        else if (view.id == R.id.do_it || view.id == R.id.palpatineGif) {
+//            playSound(R.raw.do_it_trimmed)
+//        }
+//        else if (view.id == R.id.order_66_palpatine) {
+//            playSound(R.raw.order66_palpatine)
+//        }
+//        else if (view.id == R.id.senate) {
+//            playSound(R.raw.senate_palpatine)
+//        }
+//        else if (view.id == R.id.good_palpatine) {
+//            playSound(R.raw.good_palpatine)
+//        }
+//        else if (view.id == R.id.weak_palpatine) {
+//            playSound(R.raw.too_weak_palpatine)
+//        }
+//
+//        // Vader
+//        else if (view.id == R.id.i_am_your_button || view.id == R.id.i_am_your_father_gif) {
+//            playSound(R.raw.i_am_your_sound)
+//        }
+//        else if (view.id == R.id.lack_of_faith_vader) {
+//            playSound(R.raw.faith_vader)
+//        }
+//        else if (view.id == R.id.yes_vader) {
+//            playSound(R.raw.yes_vader)
+//        }
+//        else if (view.id == R.id.fail_me_vader) {
+//            playSound(R.raw.fail_me_vader)
+//        }
+//        else if (view.id == R.id.apology_vader) {
+//            playSound(R.raw.apology_vader)
+//        }
+//        else if (view.id == R.id.noooo) {
+//            playSound(R.raw.nooo)
+//        }
 
     }
 
-//    private fun playSound(view: View, soundID: Int) {
-//        view.setOnClickListene
-//        )
-//
-//    }
+    override fun onDestroy() {
+        super.onDestroy()
+        soundPool.release()
+    }
 
     private fun playSound(soundID: Int) {
-        mediaPlayer = MediaPlayer.create(this, soundID)
-        mediaPlayer.start()
-        mediaPlayer.setOnCompletionListener {
-            onCompletion(mediaPlayer)
-        }
+
     }
 
-    private fun onCompletion(mediaPlayer: MediaPlayer?) {
-        mediaPlayer?.release()
-    }
+//    private fun onCompletion(mediaPlayer: MediaPlayer?) {
+//        mediaPlayer?.reset()
+//    }
 
+    // Navigation Menu
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         drawerLayout = findViewById(R.id.drawerLayout)
         when (item.itemId) {
@@ -155,6 +288,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }; drawerLayout.close()
         return true
     }
+
 }
 
 
--- app/src/main/java/com/hellothere/ObiWanFrag.kt
+++ app/src/main/java/com/hellothere/ObiWanFrag.kt
@@ -1,9 +1,12 @@
 package com.hellothere
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class ObiWanFrag : Fragment(R.layout.fragment_obi_wan)
